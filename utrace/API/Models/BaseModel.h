d3fc2d7a868aacba6483374e48f7e06f5fe0800c
irworks <admin@irworks.de>
2016-08-08 09:52:42 +0100

    + Added BaseModel

---
+++ utrace/API/Models/BaseModel.h
@@ -0,0 +1,13 @@
+//
+//  BaseModel.h
+//  utrace
+//
+//  Created by Ilja Rozhko on 08.08.16.
+//  Copyright © 2016 IR Works. All rights reserved.
+//
+
+#import <JSONModel/JSONModel.h>
+
+@interface BaseModel : JSONModel
+
+@end
---
+++ utrace/API/Models/BaseModel.m
@@ -0,0 +1,17 @@
+//
+//  BaseModel.m
+//  utrace
+//
+//  Created by Ilja Rozhko on 08.08.16.
+//  Copyright © 2016 IR Works. All rights reserved.
+//
+
+#import "BaseModel.h"
+
+@implementation BaseModel
+
++ (BOOL)propertyIsOptional:(NSString *)propertyName {
+    return YES;
+}
+
+@end
