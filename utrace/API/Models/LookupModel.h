5e978b877e5139b2a418969c6185c77cb6a13897
irworks <admin@irworks.de>
2016-08-08 09:58:29 +0100

    + Added Lookup model

---
+++ utrace/API/Models/LookupModel.h
@@ -0,0 +1,22 @@
+//
+//  LookupModel.h
+//  utrace
+//
+//  Created by Ilja Rozhko on 08.08.16.
+//  Copyright © 2016 IR Works. All rights reserved.
+//
+
+#import "BaseModel.h"
+#import <MapKit/MapKit.h>
+
+@interface LookupModel : BaseModel
+
+@property (strong, nonatomic, nullable) NSString <Optional> *ip;
+@property (strong, nonatomic, nullable) NSString <Optional> *hostname;
+@property (strong, nonatomic, nullable) NSString <Optional> *city;
+@property (strong, nonatomic, nullable) NSString <Optional> *region;
+@property (strong, nonatomic, nullable) NSString <Optional> *country;
+@property (nonatomic) CLLocationCoordinate2D loc;
+@property (strong, nonatomic, nullable) NSString <Optional> *orig;
+
+@end
---
+++ utrace/API/Models/LookupModel.m
@@ -0,0 +1,17 @@
+//
+//  LookupModel.m
+//  utrace
+//
+//  Created by Ilja Rozhko on 08.08.16.
+//  Copyright © 2016 IR Works. All rights reserved.
+//
+
+#import "LookupModel.h"
+
+@implementation LookupModel
+
+- (void)setLocWithNSString:(NSString *)loc {
+    NSLog(@"%@", loc);
+}
+
+@end
